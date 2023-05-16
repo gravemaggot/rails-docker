c6fd1c72623bc113ca5c7448d9022208cdd23409
gravemaggot <gravemaggot1@gmail.com>
2023-05-16 13:02:01 +0300

    Step 5: Running docker and fixing bugs for it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
 # Adding demo sharks
-sharks = Shark.create([
-    { name: 'Greate White', facts: 'Scary' },
-    { name: 'Megaldon' facts: 'Ancient'},
-    { name: 'Hammerhead' facts: 'Hammer-like'},
-    { name: 'Speartooth' facts: 'Endangered'}
-])
+sharks = Shark.create([{ name: 'Greate White', facts: 'Scary' }, { name: 'Megaldon', facts: 'Ancient'}, { name: 'Hammerhead', facts: 'Hammer-like'}, { name: 'Speartooth', facts: 'Endangered'}])
 Post.create(body: 'These sharks are misunderstood', shark: sharks.first)
